94733174a0a7c450b7d29433d3346d5308212318
Martin <110391540+albummi@users.noreply.github.com>
2025-08-14 14:45:41 +0200

    Update logo-and.js

--- logo-and.js
+++ logo-and.js
@@ -1,29 +1,12 @@
+const makeLogoLogicNode = require("./logic-core");
+
 module.exports = function(RED) {
-    function LogoAndNode(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
-        node.input1Field = config.input1Field || "topic";
-        node.input1Value = config.input1Value;
-        node.input2Field = config.input2Field || "topic";
-        node.input2Value = config.input2Value;
-
-        let state = { in1: false, in2: false };
-
-        node.on('input', function(msg) {
-            if (msg[node.input1Field] !== undefined && msg[node.input1Field] == node.input1Value) {
-                state.in1 = true;
-            } else if (msg[node.input1Field] !== undefined) {
-                state.in1 = false;
-            }
-
-            if (msg[node.input2Field] !== undefined && msg[node.input2Field] == node.input2Value) {
-                state.in2 = true;
-            } else if (msg[node.input2Field] !== undefined) {
-                state.in2 = false;
-            }
-
-            node.send({ payload: state.in1 && state.in2 });
-        });
-    }
-    RED.nodes.registerType("logo-and", LogoAndNode);
-}
+  RED.nodes.registerType(
+    "logo-and",
+    makeLogoLogicNode(RED, {
+      typeName: "logo-and",
+      displayName: "AND",
+      computeResult: (inputs) => inputs.every(Boolean)
+    })
+  );
+};
