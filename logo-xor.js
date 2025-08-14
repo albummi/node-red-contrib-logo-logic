37298ae9fc8e0dcd40e440e4afa552fb54fe2ed0
Martin <110391540+albummi@users.noreply.github.com>
2025-08-14 14:46:28 +0200

    Update logo-xor.js

--- logo-xor.js
+++ logo-xor.js
@@ -1,29 +1,16 @@
+const makeLogoLogicNode = require("./logic-core");
+
 module.exports = function(RED) {
-    function LogoXorNode(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
-        node.input1Field = config.input1Field || "topic";
-        node.input1Value = config.input1Value;
-        node.input2Field = config.input2Field || "topic";
-        node.input2Value = config.input2Value;
-
-        let state = { in1: false, in2: false };
-
-        node.on('input', function(msg) {
-            if (msg[node.input1Field] !== undefined && msg[node.input1Field] == node.input1Value) {
-                state.in1 = true;
-            } else if (msg[node.input1Field] !== undefined) {
-                state.in1 = false;
-            }
-
-            if (msg[node.input2Field] !== undefined && msg[node.input2Field] == node.input2Value) {
-                state.in2 = true;
-            } else if (msg[node.input2Field] !== undefined) {
-                state.in2 = false;
-            }
-
-            node.send({ payload: state.in1 !== state.in2 });
-        });
-    }
-    RED.nodes.registerType("logo-xor", LogoXorNode);
-}
+  RED.nodes.registerType(
+    "logo-xor",
+    makeLogoLogicNode(RED, {
+      typeName: "logo-xor",
+      displayName: "XOR",
+      computeResult: (inputs) => {
+        // true when exactly one input = true
+        const trues = inputs.reduce((c,v) => c + (v?1:0), 0);
+        return trues === 1;
+      }
+    })
+  );
+};
