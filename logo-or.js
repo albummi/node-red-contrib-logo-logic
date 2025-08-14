6b142661c54525391f857568e84e5b3e5f463655
Martin <110391540+albummi@users.noreply.github.com>
2025-08-14 14:22:19 +0200

    Update logo-or.js

--- logo-or.js
+++ logo-or.js
@@ -1,12 +1,29 @@
-const makeLogoLogicNode = require("./logic-core");
-
 module.exports = function(RED) {
-    RED.nodes.registerType(
-        "logo-or",
-        makeLogoLogicNode(RED, {
-            typeName: "logo-or",
-            displayName: "OR",
-            computeResult: (inputs) => inputs.some(Boolean)
-        })
-    );
-};
+    function LogoOrNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.input1Field = config.input1Field || "topic";
+        node.input1Value = config.input1Value;
+        node.input2Field = config.input2Field || "topic";
+        node.input2Value = config.input2Value;
+
+        let state = { in1: false, in2: false };
+
+        node.on('input', function(msg) {
+            if (msg[node.input1Field] !== undefined && msg[node.input1Field] == node.input1Value) {
+                state.in1 = true;
+            } else if (msg[node.input1Field] !== undefined) {
+                state.in1 = false;
+            }
+
+            if (msg[node.input2Field] !== undefined && msg[node.input2Field] == node.input2Value) {
+                state.in2 = true;
+            } else if (msg[node.input2Field] !== undefined) {
+                state.in2 = false;
+            }
+
+            node.send({ payload: state.in1 || state.in2 });
+        });
+    }
+    RED.nodes.registerType("logo-or", LogoOrNode);
+}
