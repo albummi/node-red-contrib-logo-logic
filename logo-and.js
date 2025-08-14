20f5e40f2832fbdd4d5912cb615cf3c67d34f9b4
Martin <110391540+albummi@users.noreply.github.com>
2025-08-14 15:29:40 +0200

    Update logo-and.js

--- logo-and.js
+++ logo-and.js
@@ -1,13 +1,8 @@
 const makeLogoLogicNode = require("./logic-core");
-
 module.exports = function(RED) {
-    RED.nodes.registerType(
-        "logo-and",
-        makeLogoLogicNode(RED, {
-            typeName: "logo-and",
-            displayName: "AND",
-            computeResult: (inputs) => inputs.every(Boolean)
-        })
-    );
+  RED.nodes.registerType("logo-and", makeLogoLogicNode(RED, {
+    typeName: "logo-and",
+    displayName: "AND",
+    computeResult: inputs => inputs.every(Boolean)
+  }));
 };
-
